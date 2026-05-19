c2830ca3f1fedeb9991228846cf812eb261844d2
Akshay Kalidatta <76524295+akshaykalidatta@users.noreply.github.com>
2026-05-19 10:29:55 +0530

    Add: rotting-fruit - submission-1

---
+++ Data Structures & Algorithms/rotting-fruit/submission-1.py
@@ -0,0 +1,42 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
+        queue = deque()
+        visited = set()
+        fresh = False
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 2:
+                    queue.append((i, j))
+                    visited.add((i, j))
+                if grid[i][j] == 1:
+                    fresh = True
+
+        if len(queue) == 0 and not fresh:
+            return 0
+
+        length = 0
+        while queue:
+            for i in range(len(queue)):
+                r, c = queue.popleft()
+                visited.add((r, c))
+                for dr, dc in directions:
+                    if (r+dr)<0 or (c+dc)<0 or (r+dr)>=ROWS or (c+dc)>=COLS or grid[r+dr][c+dc]==0 or grid[r+dr][c+dc]==2:
+                        continue
+                    if (r+dr,c+dc) not in visited:
+                        queue.append((r+dr, c+dc))
+                        visited.add((r+dr, c+dc))
+                        grid[r+dr][c+dc] = 2
+
+            length += 1
+
+        print(grid)
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 1:
+                    return -1
+
+        return length - 1
