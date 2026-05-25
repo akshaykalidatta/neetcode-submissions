f6ac52975291bd696947037cbe75cc93920c4636
Akshay Kalidatta <76524295+akshaykalidatta@users.noreply.github.com>
2026-05-25 09:55:37 +0530

    Add: swim-in-rising-water - submission-1

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        heap = []
+        visited = set()
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
+        ans = 0
+        ROWS = len(grid)
+        COLS = len(grid[0])
+
+        heapq.heappush(heap, (grid[0][0], (0,0)))
+        while heap:
+            wt, (x, y) = heapq.heappop(heap)
+            if (x, y) in visited: continue
+            ans = max(ans, wt)
+            if x==ROWS-1 and y==COLS-1: return ans
+            visited.add((x,y))
+            
+            for dr,dc in directions:
+                if (x+dr)<0 or (y+dc)<0 or (x+dr)>=ROWS or (y+dc)>=COLS:
+                    continue
+                if (x+dr, y+dc) not in visited:
+                    heapq.heappush(heap, (grid[x+dr][y+dc], (x+dr, y+dc)))
+                    
+
+        
