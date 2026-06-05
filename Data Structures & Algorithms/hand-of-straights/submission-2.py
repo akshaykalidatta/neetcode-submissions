20d3b381bec08b05ef1031c6d96cd6998bbe50f0
Akshay Kalidatta <76524295+akshaykalidatta@users.noreply.github.com>
2026-06-05 20:55:45 +0530

    Add: hand-of-straights - submission-2

---
+++ Data Structures & Algorithms/hand-of-straights/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        hmap = {}
+        for i in hand:
+            if i not in hmap:
+                hmap[i] = 1
+            else:
+                hmap[i] += 1
+
+        hand = sorted(hand)
+
+        for i in hand:
+            if hmap[i]==0:
+                continue
+            for j in range(i, i+groupSize):
+                if j not in hmap or hmap[j]==0:
+                    return False
+                hmap[j] -= 1
+
+        return True
